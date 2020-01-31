1f12d8a82e8ce355ec357f48be69b0e8a3465484
Dolphago <adamdoha@users.noreply.github.com>
2020-02-01 03:43:00 +0900

    인텔리제이 업데이트하고 이상하게 클래스 생성이 안되는 오류 발생

--- Back/Latte/src/main/java/com/latte/admin/web/dto/order/OrderResponseDto.java
+++ Back/Latte/src/main/java/com/latte/admin/web/dto/order/OrderResponseDto.java
@@ -1,4 +1,20 @@
 package com.latte.admin.web.dto.order;
 
-public class OrderResponseDto {
+import com.latte.admin.domain.cafe.Cafe;
+import com.latte.admin.domain.order.Order;
+import com.latte.admin.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OrderRequestDto {
+
+    public Order toEntity(Long ccid, Long uuid) {
+        return Order.builder()
+                .cafeorder(new Cafe(ccid))
+                .userorder(new User(uuid))
+                .build();
+    }
+
 }
